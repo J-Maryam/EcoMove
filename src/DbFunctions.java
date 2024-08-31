b17e5a64c5d7232f14b90e2cf5cac1744c9661ff
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-08-31 13:38:15 +0100

    connection with the database

--- src/DbFunctions.java
+++ src/DbFunctions.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class DbFunctions {
+
+    public Connection connectToDb( String dbName, String user, String pass) {
+        Connection con = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbName, user, pass);
+
+            if (con != null) {
+                System.out.println("Connected to PostgreSQL database");
+            }
+            else {
+                System.out.println("Failed to connect to PostgreSQL database");
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return con;
+    }
 }
