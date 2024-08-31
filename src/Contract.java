44608077029704abd98f19cfe6d5d0dab1dba51f
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-08-31 14:42:41 +0100

    update Contract class

---
+++ src/Contract.java
@@ -0,0 +1,79 @@
+import java.util.Date;
+
+public class Contract {
+
+    private int id;
+    private Date startDate;
+    private Date endDate;
+    private float specialRate;
+    private String agreementConditions;
+    private boolean renewable;
+    private ContractStatus contractStatus;
+
+    public Contract(int id, Date startDate, Date endDate, float specialRate, String agreementConditions, boolean renewable, ContractStatus contractStatus) {
+        this.id = id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.specialRate = specialRate;
+        this.agreementConditions = agreementConditions;
+        this.renewable = renewable;
+        this.contractStatus = contractStatus;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public float getSpecialRate() {
+        return specialRate;
+    }
+
+    public void setSpecialRate(float specialRate) {
+        this.specialRate = specialRate;
+    }
+
+    public String getAgreementConditions() {
+        return agreementConditions;
+    }
+
+    public void setAgreementConditions(String agreementConditions) {
+        this.agreementConditions = agreementConditions;
+    }
+
+    public boolean isRenewable() {
+        return renewable;
+    }
+
+    public void setRenewable(boolean renewable) {
+        this.renewable = renewable;
+    }
+
+    public ContractStatus getContractStatus() {
+        return contractStatus;
+    }
+
+    public void setContractStatus(ContractStatus contractStatus) {
+        this.contractStatus = contractStatus;
+    }
+
+}
