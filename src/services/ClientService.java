82acc2d600ab8bbfc53a082b678dd93fad3c0cd6
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-09-09 16:35:50 +0100

    ajout du methode pour valider l'email

--- src/services/ClientService.java
+++ src/services/ClientService.java
@@ -2,10 +2,22 @@ package services;
 
 import dao.ClientDao;
 import models.entities.Client;
+import models.entities.Partner;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ClientService {
 
     private ClientDao clientDao;
+
+    public boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9+_.-]+@(.+)$";
+
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
 
     public ClientService(ClientDao clientDao) {
         this.clientDao = clientDao;
