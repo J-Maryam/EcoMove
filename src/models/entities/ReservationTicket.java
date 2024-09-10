b79aea3312ff6555b11f527d10fcbe865895dc84
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-09-10 20:40:04 +0100

    ajout de la class ReservationTicket

--- src/models/entities/ReservationTicket.java
+++ src/models/entities/ReservationTicket.java
@@ -1,4 +1,42 @@
 package models.entities;
 
+import java.util.List;
+
 public class ReservationTicket {
+
+    private int id;
+    private List<Reservation> reservations ;
+    private List<Ticket> tickets ;
+
+    public ReservationTicket() {}
+
+    public ReservationTicket(int id, List<Reservation> reservations, List<Ticket> tickets) {
+        this.id = id;
+        this.reservations = reservations;
+        this.tickets = tickets;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
 }
