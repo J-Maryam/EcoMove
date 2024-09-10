5bb8bfec15a79e4d72584107fea687594c4d66fa
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-09-10 20:20:48 +0100

    ajout de la classe Cities

--- src/models/entities/Cities.java
+++ src/models/entities/Cities.java
@@ -1,4 +1,29 @@
 package models.entities;
 
 public class Cities {
+
+    private int id;
+    private String cityName;
+
+    public Cities(int id, String cityName) {
+        this.id = id;
+        this.cityName = cityName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
 }
